bc1ca112f73faed14855aa3a0ad63a010ed7b487
formatchou <formatc.chou@gmail.com>
2019-03-08 05:02:48 +0800

    Add files via upload

---
+++ extractor.cmd
@@ -0,0 +1,4 @@
+REM Extract named entity from unlabeled data
+java -cp NER.jar Testing.Extractor -strModel Corpus.model -strInput UnLabeledCorpus.txt -strOutput UnLabeledCorpus_Output.txt
+
+pause
---
+++ training.cmd
@@ -0,0 +1,9 @@
+REM Prepare Data
+java -cp NER.jar PrepareData.AutoLabeling -strInput_S Corpus.txt -strSeeds_S Seeds.txt -strOutput_L Corpus_L.txt -bFilterNegExamples True -bPreProcessing True
+java -cp NER.jar PrepareData.MineDict -strInput_L Corpus_L.txt -strMethod Supp -fThreshold 0.5f
+java -cp NER.jar PrepareData.GenFeature -strInput_L Corpus_L.txt -strOutput_F Corpus_F.txt -strType Training -bPreProcessing False
+
+REM Train Model via CRF++
+java -cp NER.jar Training.CRF -strInput_F Corpus_F.txt -strModel Corpus.model
+
+pause
